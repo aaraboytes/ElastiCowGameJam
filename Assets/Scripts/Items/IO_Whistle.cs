3b4c11a9840737941c3b7227983ebfc9c2366a6e
chetobros <chetobros@gmail.com>
2020-12-20 01:49:25 -0600

    World

---
+++ Assets/Scripts/Items/IO_Whistle.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IO_Whistle : InteractuableObject
+{
+    private void Start()
+    {
+        interactionType = InteractionType.Pickable;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerWhistle.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWhistle : MonoBehaviour
+{
+    [SerializeField] float _blowRadius;
+    [SerializeField] float _coolDownTime;
+    [SerializeField] PlayerItems _playerItems;
+    [SerializeField] ItemData _whistleyData;
+    [SerializeField] AudioClip _whistleySound;
+
+    private float timer = 0;
+    private AudioSource audio;
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(0, 1, 1, 0.2f);
+        Gizmos.DrawWireSphere(transform.position, _blowRadius);
+    }
+    private void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+    private void Update()
+    {
+        if (Input.GetButtonDown("Whistle") && timer<=0)
+        {
+            if (_playerItems.Items.Contains(_whistleyData))
+            {
+                _playerItems.Remove(_whistleyData);
+                Blow();
+                timer = _coolDownTime;
+            }
+        }
+        if (timer > 0)
+            timer -= Time.deltaTime;
+    }
+    private void Blow()
+    {
+        audio.PlayOneShot(_whistleySound);
+        CowsManager.Instance.ScareCows(transform.position, _blowRadius);
+    }
+}
